9b194548adba28db88bba5fb2b1705e6a9c6b4b5
Jack Knowles <jackmhknowles@gmail.com>
2022-07-10 18:21:34 +0100

    current attempt at dropdown list not working, will try different approach

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
 from models.player import *
 from models.game import *
@@ -8,11 +8,23 @@ def home():
     return render_template('home.html', title='Home')
 
 @app.route('/<p1_choice>/<p2_choice>')
-def play(p1_choice, p2_choice):
+def play_url(p1_choice, p2_choice):
     player_1 = Player("Player 1", p1_choice)
     player_2 = Player("Player 2", p2_choice)
 
     winner = Game.play(player_1.name, player_1.choice, player_2.name, player_2.choice)
     
+    
+    return render_template('results.html', title="Results", player_1=player_1, player_2=player_2, winner=winner)
+
+@app.route('/play', methods=['GET','POST'])
+def play_form():
+    p1_choice = request.form['p1-input']
+    p2_choice = request.form['p2-input']
+
+    player_1 = Player("Player 1", p1_choice)
+    player_2 = Player("Player 2", p2_choice)
+
+    winner = Game.play(player_1.name, player_1.choice, player_2.name, player_2.choice)
     
     return render_template('results.html', title="Results", player_1=player_1, player_2=player_2, winner=winner)
