63234c5aa1f035864d975b9c542b12f580da2115
Jack Knowles <jackmhknowles@gmail.com>
2022-07-10 15:29:30 +0100

    started to add CSS

--- models/game.py
+++ models/game.py
@@ -26,40 +26,4 @@ class Game():
             winner = p2_name
             
         return winner
-        
-        # if p1 == None and p2 == None:
-        #     return f"{player_1.name} and {player_2.name} draw with {p1_choice}!"
-        # elif p1 == True:
-        #     return f"{player_1.name} wins with {p1_choice}!"
-        # elif p2 == True:
-        #     return f"{player_2.name} wins with {p2_choice}!"
-        
-        
-#_________________PREVIOUS VERSIONS OF def play():
-        
-#   def play(player1_name, p1_choice, player2_name, p2_choice):
-#         p1 = None
-#         p2 = None       
-#         if p1_choice == p2_choice:
-#             p1 == None, p2 == None
-#         elif p1_choice == "rock" and p2_choice == "scissors":
-#             p1 == True
-#         elif p2_choice == "rock" and p1_choice == "scissors":
-#             p2 == True
-              
-#         elif p1_choice == "rock" and p2_choice == "paper":
-#             p2 == True
-#         elif p2_choice == "rock" and p1_choice == "paper":
-#             p1 == True        
-#         elif p1_choice == "paper" and p2_choice == "scissors":
-#             p1 == True
-#         elif p2_choice == "paper" and p1_choice == "scissors":
-#             p2 == True
-        
-#         if p1 == None and p2 == None:
-#             return f"{player1_name} and {player2_name} draw with {p1_choice}!"
-#         elif p1 == True:
-#             return f"{player1_name} wins with {p1_choice}!"
-#         elif p2 == True:
-#             return f"{player2_name} wins with {p2_choice}!"
-        
+ 
